bf832e7f51c2a57a9316246eb1e955fe4b6e6c66
Faisal Ahmed <faisaldevbhaluka@gmail.com>
2026-06-18 14:42:18 +0600

    header style done

--- header.php
+++ header.php
@@ -16,41 +16,45 @@
 
 <div id="page" class="site">
 
-<header class="site-header lsc-container layout-padding">
-	<div class="header-main-inner">
+<header class="site-header layout-padding">
+    <div class="lsc-container">
+        <div class="header-main-inner">
 		<div class="site-branding">
 			<?php lsc_render_site_logo(); ?>
 		</div>
 
-		<nav id="primary-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Main navigation', 'lsc-group' ); ?>">
-			<?php
-			wp_nav_menu( [
-				'theme_location' => 'mainMenu',
-				'container'      => false,
-				'menu_class'     => 'main-menu',
-				'fallback_cb'    => false,
-				'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-			] );
-			?>
-		</nav>
+		  <div class="header-right">
+                    <nav id="primary-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Main navigation', 'lsc-group' ); ?>">
+                        <?php
+                        wp_nav_menu( [
+                            'theme_location' => 'mainMenu',
+                            'container'      => false,
+                            'menu_class'     => 'main-menu',
+                            'fallback_cb'    => false,
+                            'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                        ] );
+                        ?>
+                    </nav>
 
-		<div class="header-actions">
-			<?php
-			if ( function_exists( 'lsc_render_header_phone' ) ) {
-				lsc_render_header_phone();
-			}
+                    <div class="header-actions">
+                        <?php
+                        if ( function_exists( 'lsc_render_header_phone' ) ) { 
+                            lsc_render_header_phone();
+                        }
 
-			if ( function_exists( 'lsc_render_header_button' ) ) {
-				lsc_render_header_button( [ 'class' => 'site-btn btn-primary header-cta-btn' ] );
-			}
-			?>
-		</div>
+                        if ( function_exists( 'lsc_render_header_button' ) ) {
+                            lsc_render_header_button( [ 'class' => 'site-btn btn-primary header-cta-btn' ] );
+                        }
+                        ?>
+                    </div>
+            </div>
 
-		<button class="mobile-menu-toggle" aria-controls="mobile-navigation" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle menu', 'lsc-group' ); ?>">
+		<button class="mobile-menu-toggle d-none" aria-controls="mobile-navigation" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle menu', 'lsc-group' ); ?>">
 			<span class="hamburger-line"></span>
 			<span class="hamburger-line"></span>
 			<span class="hamburger-line"></span>
 		</button>
 
-	</div>
+	  </div>
+    </div>
 </header>
