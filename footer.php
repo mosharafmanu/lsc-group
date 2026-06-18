61b6d19e84bb77fecc0b82121ab6132bc781c30d
Faisal Ahmed <faisaldevbhaluka@gmail.com>
2026-06-18 18:05:39 +0600

    footer style done

--- footer.php
+++ footer.php
@@ -5,53 +5,55 @@
 
 ?>
 
-<footer id="colophon" class="site-footer layout-padding">
+<footer id="colophon" class="site-footer">
+	 <div class="footer-top-inner layout-padding">
+         <div class="lsc-container">
+			 <div class="footer-top pt-60 pt-lg-65">
+				<div class="footer-brand-column">
+					<?php if ( function_exists( 'lsc_render_footer_logo' ) ) : ?>
+						<?php lsc_render_footer_logo(); ?>
+					<?php endif; ?>
 
-<div class="lsc-container">
-     <div class="footer-top pt-50 pt-lg-65">
-		<div class="footer-brand-column">
-			<?php if ( function_exists( 'lsc_render_footer_logo' ) ) : ?>
-				<?php lsc_render_footer_logo(); ?>
-			<?php endif; ?>
+					<?php if ( function_exists( 'lsc_render_footer_tagline' ) ) : ?>
+						<?php lsc_render_footer_tagline(); ?>
+					<?php endif; ?>
 
-			<?php if ( function_exists( 'lsc_render_footer_tagline' ) ) : ?>
-				<?php lsc_render_footer_tagline(); ?>
-			<?php endif; ?>
+					<?php if ( function_exists( 'lsc_render_social_medias' ) ) : ?>
+						<?php lsc_render_social_medias(); ?>
+					<?php endif; ?>
+				</div>
 
-			<?php if ( function_exists( 'lsc_render_social_medias' ) ) : ?>
-				<?php lsc_render_social_medias(); ?>
-			<?php endif; ?>
-		</div>
+				 <?php
+					 if ( function_exists( 'lsc_render_footer_menu' ) ) {
+						lsc_render_footer_menu( [ 'location' => 'footerMenu', 'title' => __( 'Home', 'lsc-group' ) ] );
+					 }
+				 ?>
 
-		<?php
-		if ( function_exists( 'lsc_render_footer_menu' ) ) {
-			lsc_render_footer_menu( [ 'location' => 'footerMenu', 'title' => __( 'Home', 'lsc-group' ) ] );
-		}
-		?>
+			       <?php if ( function_exists( 'lsc_render_footer_contact' ) ) : ?>
+						<?php lsc_render_footer_contact(); ?>
+					<?php endif; ?>
 
-		<?php if ( function_exists( 'lsc_render_footer_contact' ) ) : ?>
-			<?php lsc_render_footer_contact(); ?>
-		<?php endif; ?>
+				</div>
 
-	</div>
+				<?php if ( function_exists( 'lsc_render_footer_company_registrations' ) ) : ?>
+					<?php lsc_render_footer_company_registrations(); ?>
+				<?php endif; ?>
+			</div>
+	    </div>
+		<!--Footer Bottom-->
+		<div class="footer-bottom layout-padding">
+			   <div class="lsc-container">
+					<div class="footer-bottom-inner">
+						<?php if ( function_exists( 'lsc_render_footer_copyright' ) ) : ?>
+						    <?php lsc_render_footer_copyright(); ?>
+					    <?php endif; ?>
 
-	<?php if ( function_exists( 'lsc_render_footer_company_registrations' ) ) : ?>
-		<?php lsc_render_footer_company_registrations(); ?>
-	<?php endif; ?>
-
-	<div class="footer-bottom">
-
-		<?php if ( function_exists( 'lsc_render_footer_copyright' ) ) : ?>
-			<?php lsc_render_footer_copyright(); ?>
-		<?php endif; ?>
-
-		<?php if ( function_exists( 'lsc_render_website_credit' ) ) : ?>
-			<?php lsc_render_website_credit(); ?>
-		<?php endif; ?>
-
-	</div>
-</div>
-
+						<?php if ( function_exists( 'lsc_render_website_credit' ) ) : ?>
+							<?php lsc_render_website_credit(); ?>
+						<?php endif; ?>
+					</div>
+		      	</div>
+		  </div>
 </footer>
 
 </div><!-- #page -->
